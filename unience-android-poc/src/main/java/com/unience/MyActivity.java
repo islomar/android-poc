5d4dccef0bd7814b89aeadc6dbf0967bb099be0d
Isidro Lopez <ilopez@unience.com>
2015-05-04 15:41:57 +0200

    Use WebView: home page loads, but login fails on redirect

--- unience-android-poc/src/main/java/com/unience/MyActivity.java
+++ unience-android-poc/src/main/java/com/unience/MyActivity.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 
 public class MyActivity extends Activity {
+
+  private WebView webView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_my);
+
+    webView = (WebView) findViewById(R.id.webview);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl("https://www.unience.com");
   }
 
 
